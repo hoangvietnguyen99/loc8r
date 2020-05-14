233351d64e842ccbc3bf3954af05841e8d9bda17
Viet Nguyen Hoang <hoangvietnguyen99@gmail.com>
2020-05-14 12:46:59 +0700

    create some views

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,7 +6,7 @@ const locationsController = require('../controllers/locations');
 const othersController = require('../controllers/others');
 
 /* GET home page. */
-router.get('/', locationsController.homelist);
+router.get('/', locationsController.homeList);
 router.get('/location', locationsController.locationInfo);
 router.get('/location/review/new', locationsController.addReview);
 router.get('/about', othersController.about);
